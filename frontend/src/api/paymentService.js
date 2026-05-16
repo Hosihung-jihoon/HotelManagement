fb149b7d1d4937e433446b49115f9cbd2799e64a
nguyen <dongsynguyen510@gmail.com>
2026-05-16 09:04:32 +0700

    Payment

--- frontend/src/api/paymentService.js
+++ frontend/src/api/paymentService.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import { VietQR } from 'vietqr';
 
 const vietQR = new VietQR({
-  clientID: 'de8a0804-a76d-41e5-8ad6-31503ce7d5f4',
-  apiKey: '17c29f09-4ea2-4417-b9c2-7f020d35de42',
+  clientID: import.meta.env.VITE_VIETQR_CLIENT_ID,
+  apiKey: import.meta.env.VITE_VIETQR_API_KEY,
 });
 
 const paymentService = {
@@ -27,8 +27,8 @@ const paymentService = {
       template: 'compact'
     }, {
       headers: {
-        'x-client-id': 'de8a0804-a76d-41e5-8ad6-31503ce7d5f4', // Placeholder or from user
-        'x-api-key': '17c29f09-4ea2-4417-b9c2-7f020d35de42',    // Placeholder or from user
+        'x-client-id': import.meta.env.VITE_VIETQR_CLIENT_ID,
+        'x-api-key': import.meta.env.VITE_VIETQR_API_KEY, 
         'Content-Type': 'application/json'
       }
     });
